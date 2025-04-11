fd72766de76023c81f97948b04400c283c097a45
Elias Lackner <lackner.elias@gmail.com>
2025-04-12 00:03:29 +0200

    fix: fix administration asset filter usage

--- src/Resources/app/administration/src/module/blog-module/elements/blog-single-select/preview/index.js
+++ src/Resources/app/administration/src/module/blog-module/elements/blog-single-select/preview/index.js
@@ -1,6 +1,14 @@
 import template from './sw-cms-el-preview-blog-single-select.html.twig';
 import './sw-cms-el-preview-blog-single-select.scss';
 
+const { Filter } = Shopware;
+
 export default {
     template,
+
+    computed: {
+        assetFilter() {
+            return Filter.getByName('asset');
+        },
+    },
 };
