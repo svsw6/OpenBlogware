26741ec904cedfcd55b2c1237b20bfc00f772123
7underlines <7underlines@gmail.com>
2025-06-05 17:47:35 +0200

    Revert "Fix duplicate action in admin"
    This reverts commit 9bbe54d40b5058a7750b4117c9d870ec8c97aebd.

--- src/Resources/app/administration/src/module/blog-module/page/werkl-blog-list/index.js
+++ src/Resources/app/administration/src/module/blog-module/page/werkl-blog-list/index.js
@@ -7,7 +7,7 @@ const Criteria = Shopware.Data.Criteria;
 export default {
     template,
 
-    inject: ['repositoryFactory', 'loginService'],
+    inject: ['repositoryFactory'],
 
     mixins: [
         Mixin.getByName('salutation'),
@@ -110,12 +110,7 @@ export default {
         },
 
         duplicateBlog(item) {
-            let headers = {};
-            if (typeof this.blogEntriesRepository.getBasicHeaders === 'function') {
-                headers = this.blogEntriesRepository.getBasicHeaders();
-            } else if (Shopware.Service && typeof Shopware.Service('loginService')?.getBasicHeaders === 'function') {
-                headers = Shopware.Service('loginService').getBasicHeaders();
-            }
+            const headers = this.blogEntriesRepository.getBasicHeaders();
             this.blogEntriesRepository.httpClient
                 .post(`/_action/werkl-blog/duplicate/${item.id}`, {}, { headers })
                 .then((response) => {
